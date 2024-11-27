b0fde0a1c297036155ed624e03ec3284a9475233
Ayala888 <ayalazhl8@gmail.com>
2024-11-27 14:06:35 +0600

    hw1 array

---
+++ hw1-week13/script.js
@@ -0,0 +1,16 @@
+let footballist = ["Роналду" , "Зидан", "Неймар", "Бензема"]
+
+footballist[1] = "Месси"
+footballist[4] = "Суарес"
+footballist[5] = "Беллингем"
+footballist[6] = "Мбаппе"
+footballist[7] = "Салах"
+
+console.log(footballist);
+
+
+if(footballist.length > 5){
+    console.log("Көп");
+}else{
+    console.log("Аз");
+}
