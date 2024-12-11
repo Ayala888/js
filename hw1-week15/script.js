02a85b66796ae9fb9520cc1fab9c4ca9a52f63b7
Ayala888 <ayalazhl8@gmail.com>
2024-12-11 06:49:37 +0600

    hw1 Introduction to DOM

---
+++ hw1-week15/script.js
@@ -0,0 +1,18 @@
+//tap1
+
+let element = document.querySelector('#taqyryp')
+element.textContent = "HW: Introduction to DOM"
+
+
+//tap2
+
+let paragraph = document.getElementById('text')
+paragraph.textContent = "Zhakanbai Ayala"
+
+
+//tap3
+
+let hobbies = document.getElementsByClassName('hobby')
+hobbies[0].innerHTML = "<b>Watch Dorama</b>"
+hobbies[1].innerHTML = "<b>Cooking</b>"
+hobbies[2].innerHTML = "<b>Play chess</b>"
